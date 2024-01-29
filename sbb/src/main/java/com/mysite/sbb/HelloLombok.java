80f31b7eba14cf7774d7695cd9fe634401d3c3c1
user <user@DESKTOP-GLUOKOB>
2024-01-29 14:44:59 +0900

    1-03 스프링부트 맛보기 (3)

---
+++ sbb/src/main/java/com/mysite/sbb/HelloLombok.java
@@ -0,0 +1,22 @@
+package com.mysite.sbb;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+//@Setter
+public class HelloLombok {
+	private final String hello;
+	private final int lombok;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		HelloLombok helloLombok = new HelloLombok("헬로", 5);
+
+		System.out.println(helloLombok.getHello());
+		System.out.println(helloLombok.getLombok());
+	}
+
+}
